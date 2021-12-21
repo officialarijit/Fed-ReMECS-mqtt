b3a3ad9247e7ebced37744b9a1ab8e2deb889692
Arijit Nandi <nandiarijit99@gmail.com>
2021-12-21 23:45:51 +0100

    Delete trigger-all.sh

--- trigger-all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-konsole --noclose -e python pub1.py &
-konsole --noclose -e python pub2.py &
-konsole --noclose -e python pub3.py &
-konsole --noclose -e python pub4.py &
-konsole --noclose -e python sub.py &
